02b10b03536a155e08591cd25818960cf933a375
CallumChandler <P2717941@my365.dmu.ac.uk>
2025-05-19 14:51:19 +0100

    Rudimentary Testing Finished

--- GSO_Project/Plugins/DRS_Test/Source/DRS_Test/Public/Prc_Testing.h
+++ GSO_Project/Plugins/DRS_Test/Source/DRS_Test/Public/Prc_Testing.h
@@ -15,6 +15,14 @@ class DRS_TEST_API UPrc_Testing : public UDRS_Processor
 	GENERATED_BODY()
 
 public:
+	//----Functions for Testing----
 	bool Debug_IsRecieverPresent(UDRS_Reciever* Rrc);
 	bool Debug_IsBroadcasterPresent(UDRS_Broadcaster* Brc);
+	
+	//Gets Racers Speeds for Testing
+	TArray<int> Debug_GetRacersSpeeds() { return GetRacersSpeeds(); }
+
+	int Debug_CalcMean(TArray<int> RaceSpeeds) { return CalcMean(RaceSpeeds); }
+
+	int Debug_ProduceRaceLevel(int val) { return ProduceRaceLevel(val); }
 };
